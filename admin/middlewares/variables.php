de12181cec64ff971ea449ce9d5f7be03f9d835e
AndyDev77 <andyvespuce77550@gmail.com>
2023-03-02 16:55:34 +0100

    administration

---
+++ admin/middlewares/variables.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost/Ecole/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'access');
+
+?>
